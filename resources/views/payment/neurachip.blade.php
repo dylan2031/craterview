3644835da310f1f43587ed382cdee515f3455b37
Dylan <dellis2031@gmail.com>
2026-06-02 22:15:00 +0100

    NeuraChip copy shortened.

--- resources/views/payment/neurachip.blade.php
+++ resources/views/payment/neurachip.blade.php
@@ -2,8 +2,7 @@
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="xp-title-bar px-3 py-2 d-flex align-items-center position-relative">
-                <img src="{{ asset('images/icons/mars.svg') }}" alt="Mars Icon" class="me-2" style="width: 18px; height: 18px;">
-                <span>Extranet Explorer &copy; 3002 | NeuraChip Payments: Pay with your mind</span>
+                <span>NeuraChip: Pay with your mind</span>
                 <img src="{{ asset('images/icons/close.png') }}" alt="Close" class="position-absolute top-0 end-0" data-bs-dismiss="modal" aria-label="Close">
             </div>
             <div class="modal-body p-0">

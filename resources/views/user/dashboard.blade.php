153a0ddb82c83bfa9399f0072d0b5b6559ab88d8
Dylan <dellis2031@gmail.com>
2026-06-11 00:07:14 +0100

    Improved dashboard responsiveness

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,13 +2,41 @@
 
 @section('content')
     <div class="background-dash text-light p-1 p-md-5">
-        <h1>Your dashboard</h1>
-        @include('user.dashcat.greet')
-        <h2>Reservations</h2>
-        @include('user.dashcat.reso')
-        <h2>Member perks</h2>
-        @include('user.dashcat.perks')
-        <h2>Settings</h2>
-        @include('user.dashcat.settings')
+
+        <div class="container-fluid">
+            <div class="row g-3">
+
+                {{-- Dashboard title --}}
+                <div class="col-12">
+                    <h1>Your dashboard</h1>
+                </div>
+
+                {{-- Greet (always full width) --}}
+                <div class="col-12">
+                    @include('user.dashcat.greet')
+                </div>
+
+                {{-- Reservations --}}
+                <div class="col-12 col-lg-6">
+                    <h2>Reservations</h2>
+                    @include('user.dashcat.reso')
+                    <br>{{-- br needed for even spacing --}}
+                </div>
+
+                {{-- Perks --}}
+                <div class="col-12 col-lg-6">
+                    <h2>Member perks</h2>
+                    @include('user.dashcat.perks')
+                </div>
+
+                {{-- Settings --}}
+                <div class="col-12 col-lg-6">
+                    <h2>Settings</h2>
+                    @include('user.dashcat.settings')
+                </div>
+
+            </div>
+        </div>
+
     </div>
 @endsection

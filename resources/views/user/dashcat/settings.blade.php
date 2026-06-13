0049cc5662781a7d7afe34dd303c8777c38e806f
Dylan <dellis2031@gmail.com>
2026-06-13 10:51:33 +0100

    Reduced icon spacing on dashboard

--- resources/views/user/dashcat/settings.blade.php
+++ resources/views/user/dashcat/settings.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-wrap gap-3 my-4">
+<div class="d-flex flex-wrap gap-2 my-4">
     <a href="/dashboard/cpanel" class="dash-item">
         <img src="/images/icons/preconv/cpanel.png"
              class="img-fluid" width="64" height="64">
